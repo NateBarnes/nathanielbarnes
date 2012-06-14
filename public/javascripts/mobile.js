c1eb862e5d15c1e620cc0560bb66cd32a11b256d
Nathaniel Barnes <kerianambrai@gmail.com>
2012-06-13 23:30:13 -0700

    Added some styling and animation to the mobile site

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,1 +1,21 @@
-$.JQTouch({});
+$(document).ready(function() {
+    $("body").css("display", "none");
+ 
+    $("body").fadeIn(500);
+ 
+    $("a").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(500, redirectPage);      
+    });
+
+    $("li").click(function(event){
+        event.preventDefault();
+        linkLocation = this.children(":first").href;
+        $("body").fadeOut(500, redirectPage);      
+    });
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+});
